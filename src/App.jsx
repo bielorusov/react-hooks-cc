1089a2f12b29d7eb620fce78c6f62ca9e1237da6
Dmytro Bielorusov <dmytro.bielorusov@gmail.com>
2021-01-21 15:28:29 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,9 @@
-import React, {useState, useEffect} from 'react'
-
-// Can use react hooks outside React components
-function useLogger(value){
-  useEffect(() => {
-    console.log('Value changed: ', value)
-  }, [value])
-}
-
-function useInput(initialValue){
-  const [value, setValue] = useState(initialValue)
-
-  const onChange = event => {
-    setValue(event.target.value)
-  }
-
-  const clear = () => setValue('')
-
-  return {
-    bind: {value, onChange},
-    value,
-    clear
-  }
-}
+import React from 'react'
 
 function App() {
-  const input = useInput('')
-  useLogger(input.value)
-
   return (
     <div className={'container pt-3'}>
-      <input type='text' {...input.bind} />
-      <button className={'btn btn-warning'} onClick={input.clear}> Clear</button>
-      <hr />
-      <h1>{input.value}</h1>
+      <center>Just replace <b>App.jsx</b> with content from <b>examples/*.jsx</b></center>
     </div>
   );
 }
